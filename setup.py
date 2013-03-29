64a5e2ed3d410890e5bb24d978eeed40d6094333
Sergey Vilgelm <sergey.vilgelm@gmail.com>
2013-03-29 13:06:10 +0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='django-statcollector',
+    version='1.0',
+    description='',
+    author='Maxim Oransky',
+    author_email='maxim.oransky@gmail.com',
+    license='WTFPL',
+    url='http://github.com/sdfsdhgjkbmnmxc/django-statcollector',
+    packages=[
+        'statcollector',
+    ],
+)
+
